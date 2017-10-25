e0fe23a1d6ca2754c60b125391f3da34640917f4
Deepak Vishwakarma <deepak.vishwakarma@wwindia.com>
2017-10-25 20:45:23 +0530

    upload project on heroku2

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/project';
 
+// $config['base_url'] = 'http://localhost/project';
+$config['base_url'] = 'https://murmuring-wildwood-23106.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
