775c0858d0f2f475799c2a9dd2740117c82bdab0
Deepak Vishwakarma <deepak.vishwakarma@wwindia.com>
2017-11-02 18:19:51 +0530

    session issue

--- application/controllers/Cart.php
+++ application/controllers/Cart.php
@@ -36,7 +36,15 @@ class Cart extends CI_Controller {
 
             $product_id = array_keys($product_details);
             $product_quantity = array();
-            $data['cart_products'] = $this->Cart_model->getAddedProducts($product_id);
+            $this->db->select('product.id,product.name,product.is_featured,product.price,product.special_price,product.special_price_from,
+        product.special_price_to,(SELECT pm.image_name FROM product_images pm WHERE pm.product_id = product.id ORDER BY pm.id ASC LIMIT 1) AS image_name');
+
+        $this->db->from('product');
+        $this->db->where_in('product.id', $product_id);
+        $r = $this->db->get();
+
+            // $data['cart_products'] = $this->Cart_model->getAddedProducts($product_id);
+             $data['cart_products'] = $r->result_array();;
             echo '<pre>';
             print_r($data['cart_products']);
             echo '</pre>';
