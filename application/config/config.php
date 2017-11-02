7ca20f20883207afdd1463a06e1cc07fb078086c
Deepak Vishwakarma <deepak.vishwakarma@wwindia.com>
2017-11-02 11:53:17 +0530

    Changes in ci=ongig file

--- application/config/config.php
+++ application/config/config.php
@@ -378,12 +378,12 @@ $config['cache_query_string'] = FALSE;
 // $config['sess_time_to_update'] = 300;
 // $config['sess_regenerate_destroy'] = FALSE;
 
-$config['sess_driver'] = 'files';
-//$config['sess_driver'] = 'database';
+//$config['sess_driver'] = 'files';
+$config['sess_driver'] = 'database';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 144000;
-$config['sess_save_path'] = APPPATH . 'cache/sessions/';
-//$config['sess_save_path'] = 'ci_sessions'; 
+//$config['sess_save_path'] = APPPATH . 'cache/sessions/';
+$config['sess_save_path'] = 'ci_sessions'; 
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
