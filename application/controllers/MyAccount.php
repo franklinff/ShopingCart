59b0f1388daf6db0a5a9e084f27c80791e4183b4
Deepak Vishwakarma <deepak.vishwakarma@wwindia.com>
2017-11-02 15:17:38 +0530

    session issue

--- application/controllers/MyAccount.php
+++ application/controllers/MyAccount.php
@@ -5,7 +5,7 @@ class MyAccount extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        session_start();
+        // session_start();
         $this->load->model('My_account_model');
     } 
 
